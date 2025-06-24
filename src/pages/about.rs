8a41a3e76a49a62199fb9aa9a907101607fe7656
edpyt <edrsa1321s@gmail.com>
2025-06-24 16:23:03 +0500

    fix: add daisyui tailwind as standalone

--- src/pages/about.rs
+++ src/pages/about.rs
@@ -13,5 +13,10 @@ pub fn About() -> impl IntoView {
         <button class="btn" on:click=move |_| { set_count.set(count.get() + increment) }>
             {move || count.get()}
         </button>
+
+        <h1 class="m-8 text-4xl">"Welcome to Leptos!"</h1>
+        <button class="mx-8 h-auto p-4 border rounded-md  bg-purple-400 hover:bg-purple-700">
+            BOB
+        </button>
     }
 }
