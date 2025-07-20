1e6e98706c56ef272bf6bde2e6bec7d5b6231a46
edpyt <edrsa1321s@gmail.com>
2025-07-20 23:49:30 +0500

    refactor(blog): rounded thumbnails

--- src/pages/blog.rs
+++ src/pages/blog.rs
@@ -27,7 +27,7 @@ pub fn Blog() -> impl IntoView {
             children=move |org_post| {
                 view! {
                     <div class="py-6 flex flex-row gap-6 md:gap-10 items-start">
-                        <img class="h-25 w-25 object-cover" src=org_post.thumbnail />
+                        <img class="h-25 w-25 object-cover avatar rounded" src=org_post.thumbnail />
                         <div class="w-full flex flex-col gap-4">
                             <div class="flex justify-between">
                                 <h2 class="text-2xl font-bold">
