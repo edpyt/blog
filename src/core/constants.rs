4d3e38dff03c8c0581b397aa5c1dbb15d852bb67
edpyt <edrsa1321s@gmail.com>
2025-07-20 23:46:20 +0500

    chore: change default thumbnail

--- src/core/constants.rs
+++ src/core/constants.rs
@@ -3,7 +3,7 @@ pub const IMAGES_CDN: &str =
     "https://raw.githubusercontent.com/edpyt/blog/refs/heads/main/assets/images/";
 
 /// Fallback thumbnail for the blog homepage
-pub const DEFAULT_THUMBNAIL: &str = "pride-trans-thumb.png";
+pub const DEFAULT_THUMBNAIL: &str = "default_thumb.png";
 
 /// DateTime format constant
 pub const DATETIME_FORMAT: &str = "%Y-%m-%d %H:%M";
